b1bae16232e32ce7c7ece3c21f46c68e26b8edb3
Tiago Jesus <tiagojesus@gmail.com>
2018-12-02 09:30:57 -0200

    Organizando layout inicial para começar a colocar as funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <header className="container">
+                    <div className="row align-items-center">
+                        <div className="col-sm">
+                            <a href="http://sistemas.procon.sp.gov.br/evitesite/list/evitesites.php"
+                               target="_blank" rel="noopener noreferrer" >
+                                <img src="http://sistemas.procon.sp.gov.br/evitesite/images/procon_transparente.gif"
+                                    alt="Procom SP"/>
+                            </a>
+                        </div>
+                        <div className="col-sm-8 text-center">
+                           <h4>Fundação de Proteção e Defesa do Consumidor
+                           </h4>
+                            <p>Evite esses Sites</p>
+                        </div>
+                        <div className="col-sm">
+                            <img src="http://sistemas.procon.sp.gov.br/evitesite/images/99.gif"
+                                 alt="Procom SP"/>
+                        </div>
+                    </div>
+                    <div className="border border-dark rounded p-3 mt-2">
+                        <p>Lista de sites que devem ser evitados, pois tiveram reclamações de consumidores registrada
+                            no Procon-SP, foram notificados, não responderam ou não foram encontrados.</p>
+
+                        <p>O consumidor que tiver dúvidas pode procurar o Procon de sua cidade ou um dos canais de
+                            atendimento da Fundação.</p>
+                    </div>
+                </header>
+
+
+                <nav></nav>
+
+
+                <section className="App container mt-3">
+                    <header>
+                        <h1>Evite estes sites</h1>
+                    </header>
+                    <table  class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Site    </th>
+                                <th scope="col">Empresa/Responsável  </th>
+                                <th scope="col">CNPJ/CPF </th>
+                                <th scope="col">Inclusão</th>
+                                <th scope="col">Situação* </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">www.sitesquedeveevitar.com.br</th>
+                            <td>Nome da empresa</td>
+                            <td>0000000000000000000000</td>
+                            <td>00/00/0000</td>
+                            <td>@ativo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">www.sitesquedeveevitar.com.br</th>
+                            <td>Nome da empresa</td>
+                            <td>0000000000000000000000</td>
+                            <td>00/00/0000</td>
+                            <td>@ativo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">www.sitesquedeveevitar.com.br</th>
+                            <td>Nome da empresa</td>
+                            <td>0000000000000000000000</td>
+                            <td>00/00/0000</td>
+                            <td>@ativo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">www.sitesquedeveevitar.com.br</th>
+                            <td>Nome da empresa</td>
+                            <td>0000000000000000000000</td>
+                            <td>00/00/0000</td>
+                            <td>@ativo</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <footer></footer>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
